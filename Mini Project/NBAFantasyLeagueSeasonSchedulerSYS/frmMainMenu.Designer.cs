272d42cd92dcbfb388f643a2388904a198f264d1
mtu-adhl <Han.Lim.Oah@students.ittralee.ie>
2022-10-20 14:45:40 +0100

    221020

--- Mini Project/NBAFantasyLeagueSeasonSchedulerSYS/frmMainMenu.Designer.cs
+++ Mini Project/NBAFantasyLeagueSeasonSchedulerSYS/frmMainMenu.Designer.cs
@@ -29,6 +29,7 @@ namespace NBAFantasyLeagueSeasonSchedulerSYS
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMainMenu));
             this.SuspendLayout();
             // 
             // frmMainMenu
@@ -36,6 +37,7 @@ namespace NBAFantasyLeagueSeasonSchedulerSYS
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmMainMenu";
             this.Text = "NBA Fantasy League Season Scheduler - [Main Menu]";
             this.ResumeLayout(false);
