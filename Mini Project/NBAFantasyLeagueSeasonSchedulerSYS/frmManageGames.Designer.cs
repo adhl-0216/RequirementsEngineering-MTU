749861cf0b353ccff39f7cca48a0fcb3ff0e3ff2
mtu-adhl <Han.Lim.Oah@students.ittralee.ie>
2022-10-20 15:45:11 +0100

    added titles for main module windows

--- Mini Project/NBAFantasyLeagueSeasonSchedulerSYS/frmManageGames.Designer.cs
+++ Mini Project/NBAFantasyLeagueSeasonSchedulerSYS/frmManageGames.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmManageGames
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmManageGames";
+            this.Name = "frmManageGames";
+            this.Text = "NBA Fantasy League Season Scheduler - [Manage Games]";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Mini Project/NBAFantasyLeagueSeasonSchedulerSYS/frmManageTeams.Designer.cs
+++ Mini Project/NBAFantasyLeagueSeasonSchedulerSYS/frmManageTeams.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmManageTeams
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmManageTeams";
+            this.Name = "frmManageTeams";
+            this.Text = "NBA Fantasy League Season Scheduler - [Manage Teams]";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
