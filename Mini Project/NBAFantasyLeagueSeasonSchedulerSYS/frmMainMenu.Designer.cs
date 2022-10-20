caf7245b570fa9035e64f5f81c2f19e0a9375429
mtu-adhl <Han.Lim.Oah@students.ittralee.ie>
2022-10-20 15:23:48 +0100

    added menu strip

--- Mini Project/NBAFantasyLeagueSeasonSchedulerSYS/frmMainMenu.Designer.cs
+++ Mini Project/NBAFantasyLeagueSeasonSchedulerSYS/frmMainMenu.Designer.cs
@@ -30,7 +30,42 @@ namespace NBAFantasyLeagueSeasonSchedulerSYS
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMainMenu));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.manageTeamsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.manageGamesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adminToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.manageTeamsToolStripMenuItem,
+            this.manageGamesToolStripMenuItem,
+            this.adminToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(934, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "mnuMainMenu";
+            // 
+            // manageTeamsToolStripMenuItem
+            // 
+            this.manageTeamsToolStripMenuItem.Name = "manageTeamsToolStripMenuItem";
+            this.manageTeamsToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.manageTeamsToolStripMenuItem.Text = "Manage Teams";
+            // 
+            // manageGamesToolStripMenuItem
+            // 
+            this.manageGamesToolStripMenuItem.Name = "manageGamesToolStripMenuItem";
+            this.manageGamesToolStripMenuItem.Size = new System.Drawing.Size(101, 20);
+            this.manageGamesToolStripMenuItem.Text = "Manage Games";
+            // 
+            // adminToolStripMenuItem
+            // 
+            this.adminToolStripMenuItem.Name = "adminToolStripMenuItem";
+            this.adminToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.adminToolStripMenuItem.Text = "Admin";
             // 
             // frmMainMenu
             // 
@@ -40,15 +75,25 @@ namespace NBAFantasyLeagueSeasonSchedulerSYS
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(934, 526);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmMainMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "NBA Fantasy League Season Scheduler - [Main Menu]";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem manageTeamsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem manageGamesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adminToolStripMenuItem;
     }
 }
 
---
+++ Mini Project/NBAFantasyLeagueSeasonSchedulerSYS/frmManageGames.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NBAFantasyLeagueSeasonSchedulerSYS
+{
+    public partial class frmManageGames : Form
+    {
+        public frmManageGames()
+        {
+            InitializeComponent();
+        }
+    }
+}
