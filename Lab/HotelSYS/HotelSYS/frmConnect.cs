9f6cfb7bdd76d21db03519fb3794b05acc346bc5
ADHL <Han.Lim.Oah@students.ittralee.ie>
2023-01-19 10:15:26 +0000

    database connection

--- Lab/HotelSYS/HotelSYS/frmConnect.cs
+++ Lab/HotelSYS/HotelSYS/frmConnect.cs
@@ -1,13 +1,15 @@
 ﻿using Oracle.ManagedDataAccess.Client;
 using System;
 using System.Data;
+using System.IO;
+using System.Security;
 using System.Windows.Forms;
 
 namespace HotelSYS
 {
     public partial class frmConnect : Form
     {
-        OracleConnection conn = new OracleConnection(DBConnect.oradb);
+        OracleConnection conn;
 
         public frmConnect()
         {
@@ -16,6 +18,11 @@ namespace HotelSYS
 
         private void btnOpen_Click(object sender, EventArgs e)
         {
+            if (conn == null)
+            {
+                string connStr = File.ReadAllLines(@"C:\Users\T00229173\Desktop\connStr.txt")[0];
+                conn = new OracleConnection(connStr);
+            }
             if (conn.State == ConnectionState.Open) return;
             conn.Open();
             updateConnectionStatus();
